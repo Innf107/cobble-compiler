72926afda9c8753970893139b1d65ff69655ed7c
Innf107 <finn@famschneider.info>
2021-03-17 18:49:52 +0100

    Added Packager Pass and Time Effect

---
+++ src/Language/Cobble/Packager.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE NoImplicitPrelude, DataKinds, ConstraintKinds, KindSignatures, ScopedTypeVariables, OverloadedStrings #-}
+module Language.Cobble.Packager where
+
+import Language.Cobble.Prelude
+import Language.Cobble.MCAsm.Types
+import Language.Cobble.Util.Polysemy.Time
+
+import Codec.Archive.Zip
+
+type PackageC (r :: EffectRow) = Members '[Time] r 
+
+data DataPackOptions = DataPackOptions {
+        name::Text
+      , description::Text
+    }
+
+dataPackOptions :: Text -> Text -> DataPackOptions
+dataPackOptions = DataPackOptions
+
+makeDataPack :: forall r. (PackageC r) => DataPackOptions -> [CompiledModule] -> Sem r LByteString
+makeDataPack options ms = fromArchive <$> do
+    ia <- initialArchive
+    foldlM (\a m -> addEntryToArchive <$> makeModEntry m <*> pure a) ia ms
+    where
+        makeModEntry :: CompiledModule -> Sem r Entry
+        makeModEntry m = toEntry ("/data" </> toString (name options) </> show (compModName m)) 
+                         <$> getTime 
+                         <*> pure (encodeUtf8 (compModInstructions m))
+                         
+        initialArchive :: Sem r Archive
+        initialArchive = do
+            t <- getTime
+            pure $ addEntryToArchive (toEntry "/pack.mcmeta" t (packMcMeta options)) emptyArchive
+    -- TODO: Include data/minecraft/tags/functions/load.mcfunction, clean module and init module
+
+packMcMeta :: DataPackOptions -> LByteString
+packMcMeta options = mconcat [
+      "{"
+    , "    \"pack\":{"
+    , "         \"pack_format\":6"
+    , "         \"description\": \"" <> encodeUtf8 (description options) <> "\""
+    , "    }"
+    , "}"
+    ]
+
---
+++ src/Language/Cobble/Util/Polysemy/Time.hs
@@ -0,0 +1,33 @@
+{-#LANGUAGE NoImplicitPrelude, TemplateHaskell, DataKinds, PolyKinds, TypeOperators, FlexibleContexts#-}
+{-# LANGUAGE GADTs, ScopedTypeVariables, LambdaCase, BlockArguments #-}
+module Language.Cobble.Util.Polysemy.Time (
+      Time
+    , getTime
+    , timeToIO
+    , runTimePredefined
+    , getTimeIO
+    ) where
+
+import Language.Cobble.Prelude
+
+import Data.Time
+import Data.Time.Clock.POSIX
+
+data Time m a where
+    GetTime :: Time m Integer
+
+makeSem ''Time
+
+timeToIO :: Member (Embed IO) r => Sem (Time ': r) a -> Sem r a
+timeToIO = interpret \case
+    GetTime -> embed $ getTimeIO
+
+runTimePredefined :: Integer -> Sem (Time ': r) a -> Sem r a
+runTimePredefined t = interpret \case
+    GetTime -> pure t
+
+getTimeIO :: IO Integer
+getTimeIO = round . nominalDiffTimeToSeconds <$> getPOSIXTime
+
+
+
