66dcb0b5c6340447d5f22555cb9ddec0f465457c
Innf107 <finn@famschneider.info>
2021-06-21 21:19:01 +0200

    Add LC pretty printer

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Cobble
-A language that compiles to minecraft datapacks
+# Cobble - A purely functional high level programming language that compiles to minecraft datapacks
+
+## Quick Start
+Right now, the only way to install cobble is by building from source.
+
+### Prerequisites
+To install cobble, you need to have [haskell stack](https://docs.haskellstack.org/en/stable/README/) as well as [zlib](https://www.zlib.net/) installed.
+
+### Build steps
+First, clone the repository. 
+```bash
+$ git clone https://github.com/Innf107/cobble-compiler
+```
+If you do not have git installed, just download the repository by clicking on **Code -> Download ZIP** and unzip the archive.
+
+Once you have cloned the repository, build and install the project using stack. (This might take a while)
+```bash
+$ stack install
--- src/Language/Cobble/LC/PrettyPrint.hs
+++ src/Language/Cobble/LC/PrettyPrint.hs
@@ -3,9 +3,29 @@ module Language.Cobble.LC.PrettyPrint where
 import Language.Cobble.Prelude
 import Language.Cobble.LC.Types
 
+import qualified Data.Text as T
+
 prettyPrintLCDefs :: [LCDef] -> Text
-prettyPrintLCDefs defs = 
+prettyPrintLCDefs = T.intercalate "\n\n" . map prettyPrintLCDef
 
 prettyPrintLCDef :: LCDef -> Text
-prettyPrintLCDef (LCDef name expr) = name <> " = " <> prettyPrintLCExpr expr
+prettyPrintLCDef (LCDef name expr) = show name <> " = " <> prettyPrintLCExpr expr
+
+prettyPrintLCExpr :: LCExpr -> Text
+prettyPrintLCExpr = \case
+    Var name            -> show name
+    Fix vname expr      -> "fix (λ" <> show vname <> ". " <> prettyPrintLCExpr expr <> ")"
+    Lambda vname expr   -> "λ" <> show vname <> ". " <> prettyPrintLCExpr expr
+    App fexpr aexpr     -> prettyPrintLCExprParens fexpr <> " " <> prettyPrintLCExprParens aexpr
+    IntLit i            -> show i
+    Tuple as            -> "[" <> T.intercalate ", " (map prettyPrintLCExpr as) <> "]"
+    SelectTuple i t     -> prettyPrintLCExprParens t <> "._" <> show i
+
+prettyPrintLCExprParens :: LCExpr -> Text
+prettyPrintLCExprParens = \case
+    Var name -> prettyPrintLCExpr (Var name)
+    Tuple as -> prettyPrintLCExpr (Tuple as)
+    IntLit i -> prettyPrintLCExpr (IntLit i)
+    x -> "(" <> prettyPrintLCExpr x <> ")"
+
 
