6ba12ba3a41368127991e99277dbb1a29026056d
Prophet <finn@famschneider.info>
2022-03-25 18:54:37 +0100

    Rewrite Lexer to insert Dedents and BlockEnds

--- src/Language/Cobble/Parser/Tokenizer.hs
+++ src/Language/Cobble/Parser/Tokenizer.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoOverloadedStrings, OverloadedLists #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Language.Cobble.Parser.Tokenizer where
 
 import Language.Cobble.Prelude hiding ((|>))
@@ -10,6 +10,8 @@ import Data.DList (DList)
 import Data.DList qualified as D
 
 import Text.Read (read)
+
+import Data.Text qualified as T
 
 (|>) :: DList a -> a -> DList a
 (|>) = D.snoc
@@ -36,37 +38,38 @@ isOpStart :: Char -> Bool
 isOpStart c = c /= '-' && isOpLetter c
 
 isOpLetter :: Char -> Bool
-isOpLetter = flip elem "+-*/~^!?.|<>$&=#:;,\\"
+isOpLetter = flip elem ("+-*/~^!?.|<>$&=#:;,\\" :: String)
 
 isIntStart :: Char -> Bool
 isIntStart x = isDigit x
 
 isIdentStart :: Char -> Bool
-isIdentStart c = isAlpha c || c `elem` "_"
+isIdentStart c = isAlpha c || c `elem` ("_" :: String)
 
 isIdentLetter :: Char -> Bool
-isIdentLetter c = isAlphaNum c || c `elem` "_"
+isIdentLetter c = isAlphaNum c || c `elem` ("_" :: String)
 
-reserved :: Set String
+reserved :: Set Text
 reserved = fromList ["let", "in", "if", "then", "else", "module", "import", "struct", "variant", "case", "of", "class", "instance", "infixl", "infixr", "forall", "do"]
 
-reservedOps :: Set String
+reservedOps :: Set Text
 reservedOps = fromList [".", "::", ";", ",", "=", "=>", "->", "|", "\\"]
 
-canStartBlock :: Set String
+canStartBlock :: Set Text
 canStartBlock = fromList ["do", "of", "then", "else", "="]
 
 isParen :: Char -> Bool
-isParen = (`elem`"()[]{}")
+isParen = (`elem`("()[]{}" :: String))
 
 isNumPrefix :: Char -> Bool
-isNumPrefix c = c `elem` "+-"
+isNumPrefix c = c `elem` ("+-" :: String)
 
 data LexicalError = LexicalError SourcePos FileName LexicalErrorData deriving (Show, Eq)
 
 data LexicalErrorData = ReachedEOF TokenState
-                  | UnexpectedChar Char (TokenState)
-                  deriving (Show, Eq)
+                      | UnexpectedChar Char (TokenState)
+                      | ClosedNonexistentBlock
+                      deriving (Show, Eq)
 
 data TokenState = Default
                 | InIdent (DList Char)
@@ -85,99 +88,152 @@ sameTag LeadingMinus    LeadingMinus    = True
 sameTag InComment       InComment       = True
 sameTag _               _               = False
 
-tokenize :: forall r. (Members '[Error LexicalError] r) => FileName -> Text -> Sem r [Token]
-tokenize fp content = D.toList <$> go (SourcePos 1 1) (SourcePos 1 1) [0] Default (toString content)
+data LexState = LexState {
+    _lexStartPos   :: SourcePos
+,   _lexPos        :: SourcePos
+,   _tokenState    :: TokenState
+,   _activeBlocks  :: [SourcePos]
+,   _startingBlock :: Bool
+,   _skipDedent  :: Bool
+}
+makeLenses ''LexState
+
+tokenize :: Members '[Error LexicalError] r => FileName -> Text -> Sem r [Token]
+tokenize fileName content = fmap fst 
+                          $ runOutputList 
+                          $ evalState (LexState (SourcePos 1 1) (SourcePos 1 1) Default [] False True) 
+                          $ go content
     where
-        go :: SourcePos -> SourcePos -> [Int] -> TokenState -> [Char] -> Sem r (DList Token)
-        go startPos endPos activeBlocks tokState' cs' = case (tokState', cs') of
-            (Default, []) -> pure []
-            (Default, (c:_))
-                | isWhiteSpace c -> go' Default activeBlocks
-                | isIdentStart c -> go' (InIdent [c]) activeBlocks
-                | isOpStart c    -> go' (InOp [c]) activeBlocks
-                | isIntStart c   -> go' (InIntLit [c]) activeBlocks
-                | isParen c      -> Paren [c] ++> go' Default activeBlocks
-                | c == '-'       -> go' LeadingMinus activeBlocks
-            (InIdent ident, []) -> done (Ident (toText ident))
-            (InIdent ident, (c:_))
-                | isIdentLetter c -> go' (InIdent (ident |> c)) activeBlocks
-                | isWhiteSpace c  -> mkIdent ident +|> go' Default
-                | isOpStart c     -> mkIdent ident +|> go' (InOp [c]) 
-                | isParen c       -> mkIdent ident +|> \blocks -> Paren (toText c) ++> go' Default blocks
-                | c == '-'        -> go' LeadingMinus activeBlocks
-            (InOp op, []) -> done (mkOperator op)
-            (InOp op, (c:_))
-                | isOpLetter c   -> go' (InOp (op |> c)) activeBlocks
-                | isWhiteSpace c -> mkOperator op +> go' Default activeBlocks
-                | isIdentStart c -> mkOperator op +> go' (InIdent [c]) activeBlocks
-                | isIntStart c   -> mkOperator op +> go' (InIntLit [c]) activeBlocks
-                | isParen c      -> mkOperator op +> Paren (toText c) ++> go' Default activeBlocks
-            (InIntLit lit, []) -> done (mkIntLit lit)
-            (InIntLit lit, (c:_))
-                | isDigit c      -> go' (InIntLit (lit |> c)) activeBlocks
-                | isWhiteSpace c -> mkIntLit lit +> go' Default activeBlocks
-                | isIdentStart c -> mkIntLit lit +> go' (InIdent [c]) activeBlocks
-                | isOpStart c    -> mkIntLit lit +> go' (InOp [c]) activeBlocks
-                | isParen c      -> mkIntLit lit +> Paren (toText c) ++> go' Default activeBlocks
-                | c == '-'       -> mkIntLit lit +> go' LeadingMinus activeBlocks
-            (LeadingMinus, []) -> done (mkOperator ['-'])
-            (LeadingMinus, (c:_))
-                | c == '-'       -> go' InComment activeBlocks
-                | isOpLetter c   -> go' (InOp ['-', c]) activeBlocks
-                | isWhiteSpace c -> mkOperator ['-'] +> go' Default activeBlocks
-                | isIntStart c   -> go' (InIntLit ['-', c]) activeBlocks
-                | isParen c      -> mkOperator ['-'] +> Paren [c] ++> go' Default activeBlocks
-                | isIdentStart c -> mkOperator ['-'] +> go' (InIdent [c]) activeBlocks
-            (InComment, []) -> pure []
-            (InComment, (c:_))
-                | isNewline c   -> go' Default activeBlocks
-                | otherwise     -> go' InComment activeBlocks
-            (ts, (c:_)) -> throw' (UnexpectedChar c ts)
-            where
-                go' :: TokenState -> [Int] -> Sem r (DList Token)
-                go' ts activeBlocks = do
-                    (endPos', cs) <- case cs' of
-                        []        -> throw' (ReachedEOF ts)
-                        ('\n':cs) -> pure (SourcePos{line=line endPos + 1, column=1}, cs)
-                        (_:cs)    -> pure (endPos{column=column endPos + 1}, cs)
-                    go (if sameTag ts tokState' then startPos else endPos) endPos' activeBlocks ts cs
+        go :: Members '[State LexState, Error LexicalError, Output Token] r => Text -> Sem r ()
+        go content = case T.uncons content of
+            Just (c, rest) -> do
+                when (not $ isWhiteSpace c) $ tryInsertDedent
+                lex c
+                increasePos c
+                go rest
+            -- EOF
+            Nothing -> do
+                increasePos '\n'
+                tryInsertDedent
+                lexEOF
+        
+        lex :: Members '[State LexState, Error LexicalError, Output Token] r => Char -> Sem r ()
+        lex c = gets _tokenState >>= \s -> case (s, c) of
+            (Default, c)
+                | isWhiteSpace c    -> pure ()
+                | isIdentStart c    -> setState (InIdent [c])
+                | isOpStart c       -> setState (InOp [c])
+                | isIntStart c      -> setState (InIntLit [c])
+                | isParen c         -> outputToken (Paren [c])
+                | c == '-'          -> setState LeadingMinus
+            (InIdent ident, c)
+                | isWhiteSpace c    -> emitIdent ident >> setState Default
+                | isIdentLetter c   -> setState (InIdent (ident |> c))
+                | isOpStart c       -> emitIdent ident >> setState (InOp [c])
+                | isParen c         -> emitIdent ident >> outputToken (Paren [c]) >> setState Default
+                | c == '-'          -> emitIdent ident >> setState LeadingMinus
+            (InOp op, c)
+                | isWhiteSpace c    -> emitOp op >> setState Default
+                | isOpLetter c      -> setState (InOp (op |> c))
+                | isIdentStart c    -> emitOp op >> setState (InIdent [c])
+                | isIntStart c      -> emitOp op >> setState (InIntLit [c])
+                | isParen c         -> emitOp op >> outputToken (Paren [c]) >> setState Default
+            (InIntLit lit, c)
+                | isWhiteSpace c    -> emitIntLit lit >> setState Default
+                | isDigit c         -> setState (InIntLit (lit |> c))
+                | isIdentStart c    -> emitIntLit lit >> setState (InIdent [c])
+                | isOpStart c       -> emitIntLit lit >> setState (InOp [c])
+                | isParen c         -> emitIntLit lit >> outputToken (Paren [c]) >> setState Default
+                | c == '-'          -> emitIntLit lit >> setState LeadingMinus
+            (LeadingMinus, c)
+                | isWhiteSpace c    -> emitOp ['-'] >> setState Default
+                | isIdentLetter c   -> emitOp ['-'] >> setState (InIdent [c])
+                | isOpLetter c      -> setState (InOp ['-', c])
+                | isIntStart c      -> setState (InIntLit ['-', c])
+                | isParen c         -> emitOp ['-'] >> outputToken (Paren [c]) >> setState Default
+                | c == '-'          -> setState InComment
+            (InComment, c)
+                | isNewline c       -> setState Default
+                | otherwise         -> pure ()
+            (s, c) -> throwLex $ UnexpectedChar c s
+           
+        lexEOF :: Members '[State LexState, Error LexicalError, Output Token] r => Sem r ()
+        lexEOF = gets _tokenState >>= \case
+            Default         -> pure ()
+            InIdent ident   -> emitIdent ident
+            InOp op         -> emitOp op
+            InIntLit lit    -> emitIntLit lit
+            LeadingMinus    -> emitOp ['-']
+            InComment       -> pure ()
 
-                -- | Continue by adding td to the output stream
-                (+>) :: TokenData -> Sem r (DList Token) -> Sem r (DList Token)
-                td +> rest = D.cons (Token (LexInfo startPos endPos fp) td) <$> rest
-                infixr 5 +>
-                -- | Just like (+>), but increases the endPos column for the added token by one
-                (++>) :: TokenData -> Sem r (DList Token) -> Sem r (DList Token)
-                td ++> rest = D.cons (Token (LexInfo endPos endPos{column=column endPos + 1} fp) td) <$> rest
-                infixr 5 ++>
-                
-                -- | Just like (+>), but has the ability to introduce a new block
-                (+|>) :: (TokenData, Maybe Int) -> ([Int] -> Sem r (DList Token)) -> Sem r (DList Token)
-                (td, Nothing)    +|> rest = td +> rest activeBlocks
-                (td, Just block) +|> rest = td +> rest (block : activeBlocks)
 
-                done :: TokenData -> Sem r (DList Token)
-                done = pure . pure . Token (LexInfo startPos endPos fp)
+        emitIdent :: Members '[State LexState, Output Token] r => DList Char -> Sem r ()
+        emitIdent (toText -> ident)
+            | ident `member` reserved = do
+                outputToken (Reserved ident)
+                when (ident `member` canStartBlock) $ introduceBlock
+            | otherwise = outputToken (Ident ident)
 
-                throw' :: LexicalErrorData -> Sem r a
-                throw' = throw . LexicalError endPos fp
+        emitOp :: Members '[State LexState, Output Token] r => DList Char -> Sem r ()
+        emitOp (toText -> op)
+            | op `member` reservedOps = do
+                outputToken (ReservedOp op)
+                when (op `member` canStartBlock) $ introduceBlock
+            | otherwise = outputToken (Operator op)
 
-                mkIdent :: DList Char -> (TokenData, Maybe Int)
-                mkIdent (D.toList -> idStr)
-                    | idStr `member` reserved = (Reserved (toText idStr), whenAlt (idStr `member` canStartBlock) (column startPos))
-                    | otherwise = (Ident (toText idStr), Nothing)
 
-                mkOperator :: DList Char -> TokenData
-                mkOperator (D.toList -> opStr)
-                    | opStr `member` reservedOps = ReservedOp (toText opStr)
-                    | otherwise = Operator (toText opStr)
+        emitIntLit :: Members '[State LexState, Output Token] r => DList Char -> Sem r ()
+        emitIntLit = outputToken . IntLiteral . read . D.toList -- `read` is safe here, since any constructed int lit should be valid
 
-                mkIntLit :: DList Char -> TokenData
-                mkIntLit dl = IntLiteral $ read $ D.toList dl
+        introduceBlock :: Members '[State LexState] r => Sem r ()
+        introduceBlock = modify (startingBlock .~ True)
+
+        increasePos :: Members '[State LexState, Error LexicalError] r => Char -> Sem r ()
+        increasePos '\n' = modify (lexPos %~ \(SourcePos line column) -> SourcePos (line + 1) 1)
+        increasePos _    = modify (lexPos %~ \(SourcePos line column) -> SourcePos line (column + 1))
+
+        tryInsertDedent :: Members '[State LexState, Output Token, Error LexicalError] r => Sem r ()
+        tryInsertDedent = do
+            LexState{_lexPos=sourcePos, _startingBlock, _skipDedent} <- get
+            when _startingBlock do
+                modify (activeBlocks %~ (sourcePos :))
+                modify (startingBlock .~ False)
+            gets _activeBlocks >>= \case
+                (block : remainingBlocks) -> do
+                    when (line sourcePos > line block) do
+                        when (column sourcePos == column block) $ outputToken Dedent
+                        when (column sourcePos <  column block) do
+                            outputToken BlockEnd
+                            modify (skipDedent .~ True)
+                            modify (activeBlocks .~ remainingBlocks)
+                            tryInsertDedent -- Try again, since we might close multiple blocks at once.
+
+                _ -> when (column sourcePos == 1) $ 
+                        if not _skipDedent then 
+                            outputToken Dedent
+                        else
+                            modify (skipDedent .~ False)
+            
+
+        outputToken :: Members '[State LexState, Output Token] r => TokenData -> Sem r ()
+        outputToken tokData = do
+            LexState{_lexStartPos, _lexPos} <- get
+
+            modify (lexStartPos .~ _lexPos)
+
+            output (Token (LexInfo _lexStartPos _lexPos fileName) tokData)
+
+        setState :: Members '[State LexState] r => TokenState -> Sem r ()
+        setState tokState = modify (tokenState .~ tokState)
+
+        throwLex :: Members '[State LexState, Error LexicalError] r => LexicalErrorData -> Sem r a
+        throwLex errData = do
+            sourcePos <- gets _lexPos
+            throw (LexicalError sourcePos fileName errData)
 
 
 isNewline :: Char -> Bool
 isNewline = (=='\n')
 
 isWhiteSpace :: Char -> Bool
-isWhiteSpace c = c `elem` " \t\n"
+isWhiteSpace c = c `elem` (" \t\n" :: String)
